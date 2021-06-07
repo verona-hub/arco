e0a458d01a43ea81bab1cf01741ccc41307d37c0
verona-hub <68257343+verona-hub@users.noreply.github.com>
2021-06-07 02:44:35 +0200

    minor changes

--- shell/setWallpaper.sh
+++ shell/setWallpaper.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-set -e
+# set -e
 
 
 #----------------------------------------------------------------------------------
 
-# Cleanup trash folder:
+# Setting the wallpaper:
 echo $'\n'
 tput setaf 4
 echo "Changing the wallpaper..." | cowsay | lolcat
@@ -12,17 +12,27 @@ echo "*******************************"
 echo "*******************************"
 tput sgr0
 
-# while :; do
-#    display -window root "$(find ~/pictures/wps -type f | shuf -n1)"
-#    sleep 15m
-# done &
+# while [ "$DISPLAY" == ":0.0" ]
+# do
+#     display -window root ~/Pictures/Astronaut-in-the-ocean.jpg
+#     sleep 30m
+# done
 
-feh --bg-scale /path/to/wallpaper
+# feh --bg-scale ~/Pictures/Astronaut-in-the-ocean.jpg
+  feh --bg-scale /home/admin/Pictures/Space.jpg
 
-# feh --bg-fill /path/to/background_image
 
 
-# xsetroot -bitmap filename.bmp
+# feh --bg-fill /home/admin/Pictures/Astronaut-in-the-ocean.jpg
+# feh --bg-max /home/admin/Pictures/Astronaut-in-the-ocean.jpg
+
+# xsetroot -bitmap ~/Pictures/Astronaut-in-the-ocean.jpg
+
+# xloadimage -onroot -fullscreen ~/Pictures/Astronaut-in-the-ocean.jpg
+
+# display -window root ~/Pictures/Astronaut-in-the-ocean.jpg
+# (requires ImageMagic)
+
 
 
 #----------------------------------------------------------------------------------
