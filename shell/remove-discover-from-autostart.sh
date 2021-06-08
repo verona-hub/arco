e7c254b55d0a02cce1b03599c136fe8fb1b604b6
verona-hub <68257343+verona-hub@users.noreply.github.com>
2021-06-08 03:36:16 +0200

    setWallpaper.sh modifications

--- shell/remove-discover-from-autostart.sh
+++ shell/remove-discover-from-autostart.sh
@@ -9,9 +9,10 @@ tput setaf 4
 echo "Removing Discover from autostart" | cowsay | lolcat
 echo "********************************************"
 echo "********************************************"
+echo $'\n'
 tput sgr0
-sudo rm -rv /etc/xdg/autostart/org.kde.discover.notifier.desktop
 
+sudo rm -rv /etc/xdg/autostart/org.kde.discover.notifier.desktop
 
 tput setaf 5
 echo $'\n'
--- shell/setWallpaper.sh
+++ shell/setWallpaper.sh
@@ -1,38 +1,58 @@
 #!/bin/bash
-# set -e
-
+set -e
 
 #----------------------------------------------------------------------------------
 
-# Setting the wallpaper:
+backupFolder="backup-wallpaper-settings-$(date +%Y.%m.%d-%H.%M.%S)"
+parentdir="$(dirname "$PWD")"
+
+# Change the wallpaper:
 echo $'\n'
 tput setaf 4
-echo "Changing the wallpaper..." | cowsay | lolcat
-echo "*******************************"
-echo "*******************************"
+echo "Mooo! I will change your wallpaper..." | cowsay | lolcat
+echo "**************************************"
+echo "**************************************"
 tput sgr0
 
-# while [ "$DISPLAY" == ":0.0" ]
-# do
-#     display -window root ~/Pictures/Astronaut-in-the-ocean.jpg
-#     sleep 30m
-# done
+echo "1) Creating a backup folder inside root"
+mkdir -p ~/$backupFolder
+echo
 
-# feh --bg-scale ~/Pictures/Astronaut-in-the-ocean.jpg
-  feh --bg-scale /home/admin/Pictures/Space.jpg
+echo "2) Moving to parent directory where wallpapers folder is located"
+cd $parentdir
+echo
 
+echo "3) Copying the new wallpapers to the local Pictures folder"
+cp -arf $PWD/wallpapers/Astronaut-in-the-ocean.jpg ~/Pictures
+cp -arf $PWD/wallpapers/Space-Plasma-4K.jpg ~/Pictures
+echo
+echo $'\n'
 
+echo "4) Copying the original file location .txt"
+cp -arf $PWD/wallpapers/setwallper-location ~/Pictures
+echo
+echo $'\n'
 
-# feh --bg-fill /home/admin/Pictures/Astronaut-in-the-ocean.jpg
-# feh --bg-max /home/admin/Pictures/Astronaut-in-the-ocean.jpg
+echo "5) Making a backup of the original setting file inside /.local/share/kservices5/ServiceMenus"
+cp -Rf ~/.local/share/kservices5/ServiceMenus/. ~/$backupFolder
+echo
 
-# xsetroot -bitmap ~/Pictures/Astronaut-in-the-ocean.jpg
+echo "6) Copying the new setting file to the local system"
+cp -arf $PWD/wallpapers/SetAsWallpaper-20200109222008.desktop ~/.local/share/kservices5/ServiceMenus/
+echo
+echo $'\n'
 
-# xloadimage -onroot -fullscreen ~/Pictures/Astronaut-in-the-ocean.jpg
+#----------------------------------------------------------------------------------
 
-# display -window root ~/Pictures/Astronaut-in-the-ocean.jpg
-# (requires ImageMagic)
-
-
+tput setaf 5
+echo "**************************************************"
+echo "  --- --- ---        Finished!         --- --- ---"
+echo "**************************************************"
+echo "  Backup has been made and files have been moved! "
+echo "**************************************************"
+echo "     Restart your system to apply the changes!    "
+echo "**************************************************"
+tput sgr0
+echo $'\n'
 
 #----------------------------------------------------------------------------------
