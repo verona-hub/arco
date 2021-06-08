05fceb34be0569da957e00325de32f72cd523d65
verona-hub <68257343+verona-hub@users.noreply.github.com>
2021-06-08 02:45:30 +0200

    created remove-discover-from-autostart

--- shell/clean-up-system.sh
+++ shell/clean-up-system.sh
@@ -10,7 +10,7 @@ echo "Cleaning the trash folder inside .local/" | cowsay | lolcat
 echo "********************************************"
 echo "********************************************"
 tput sgr0
-sudo sudo rm -rv ~/.local/share/Trash/expunged/*
+sudo rm -rv ~/.local/share/Trash/expunged/*
 
 
 ## Install bleachbit from AUR
---
+++ shell/remove-discover-from-autostart.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+set -e
+
+#----------------------------------------------------------------------------------
+
+# Remove Discover from autostart:
+echo $'\n'
+tput setaf 4
+echo "Removing Discover from autostart" | cowsay | lolcat
+echo "********************************************"
+echo "********************************************"
+tput sgr0
+sudo rm -rv /etc/xdg/autostart/org.kde.discover.notifier.desktop
+
+
+tput setaf 5
+echo $'\n'
+echo "*************************************************"
+echo " --- --- ---       Finished!       --- --- --- --"
+echo "*************************************************"
+tput sgr0
+echo $'\n'
+
+
+#----------------------------------------------------------------------------------
