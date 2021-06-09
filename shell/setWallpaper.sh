7081c5e24fc27e22d5b8b986c16122b377ce9e64
verona-hub <68257343+verona-hub@users.noreply.github.com>
2021-06-09 04:00:22 +0200

    updated setWallpaper.sh

--- shell/setWallpaper.sh
+++ shell/setWallpaper.sh
@@ -18,31 +18,64 @@ echo "1) Creating a backup folder inside root"
 mkdir -p ~/$backupFolder
 echo
 
-echo "2) Moving to parent directory where wallpapers folder is located"
+echo "2) Making a backup of the original file: admin/.config/plasma-org.kde.plasma.desktopappletsrc"
+cp -Rf /home/admin/.config/plasma-org.kde.plasma.desktopappletsrc ~/$backupFolder
+echo
+
+echo "3) Moving to parent directory where wallpapers folder is located"
 cd $parentdir
 echo
 
-echo "3) Copying the new wallpapers to the local Pictures folder"
-cp -arf $PWD/wallpapers/Astronaut-in-the-ocean.jpg ~/Pictures
-cp -arf $PWD/wallpapers/Space-Plasma-4K.jpg ~/Pictures
+echo "4) Copying the new wallpapers to the local Pictures folder"
+cp -arf $PWD/wallpapers/. ~/Pictures
 echo
 echo $'\n'
 
-echo "4) Copying the original file location .txt"
-cp -arf $PWD/wallpapers/setwallper-location ~/Pictures
+echo "5) Select which wallpaper you would like to set as background:"
+echo
+echo "0.  Do nothing"
+echo "1.  Space Plasma"
+echo "2.  Astronaut in the ocean"
+echo "Type the number..."
+
+read CHOICE
+
+case $CHOICE in
+
+    0 )
+      echo
+      echo "*******************************"
+      echo " --- No changes were made! --- " | cowsay | lolcat
+      echo "*******************************"
+      echo
+      ;;
+
+    1 )
+			sed -i '/Image=file/c\Image=file:///home/admin/Pictures/Space-Plasma-4K.jpg' /home/admin/.config/plasma-org.kde.plasma.desktop-appletsrc
+      ;;
+    
+    2 )
+  		sed -i '/Image=file/c\Image=file:///home/admin/Pictures/Astronaut-in-the-ocean.jpg' /home/admin/.config/plasma-org.kde.plasma.desktopappletsrc
+      ;;
+    
+    * )
+    
+      echo "*********************************"
+      echo " -- Choose the correct number -- " | cowsay | lolcat
+      echo "*********************************"
+      ;;
+esac
+
+tput setaf 4
+echo $'\n'
+echo "4) Adding the /home/admin/Pictures folder path to admin/.config/plasma-org.kde.plasma.desktopappletsrc"
+sed -i '/SlidePaths/s/$/,\/home\/admin\/Pictures/' /home/admin/.config/plasma-org.kde.plasma.desktop-appletsrc
 echo
 echo $'\n'
+tput sgr0
 
-echo "5) Making a backup of the original setting file inside /.local/share/kservices5/ServiceMenus"
-cp -Rf ~/.local/share/kservices5/ServiceMenus/. ~/$backupFolder
-echo
 
-echo "6) Copying the new setting file to the local system"
-cp -arf $PWD/wallpapers/SetAsWallpaper-20200109222008.desktop ~/.local/share/kservices5/ServiceMenus/
-echo
-echo $'\n'
-
-#----------------------------------------------------------------------------------
+#--------------------------------------------------------------------------------------------------------------
 
 tput setaf 5
 echo "**************************************************"
@@ -55,4 +88,4 @@ echo "**************************************************"
 tput sgr0
 echo $'\n'
 
-#----------------------------------------------------------------------------------
+#--------------------------------------------------------------------------------------------------------------
